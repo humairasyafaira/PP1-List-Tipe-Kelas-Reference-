b8bae0b923a4e27305ca9209bff799a48dc4e904
humairasyafaira <117334650+humairasyafaira@users.noreply.github.com>
2024-04-01 19:02:00 +0700

    Add files via upload

---
+++ Tugas7_s/Mahasiswa.java
@@ -0,0 +1,40 @@
+package Tugas7_s;
+
+public class Mahasiswa {
+	private String nrp;
+    private String nama;
+    private String tanggalLahir;
+
+    // Konstruktor
+    public Mahasiswa(String nrp, String nama, String tanggalLahir) {
+        this.nrp = nrp;
+        this.nama = nama;
+        this.tanggalLahir = tanggalLahir;
+    }
+
+    // Getter dan setter
+    public String getNrp() {
+        return nrp;
+    }
+
+    public void setNrp(String nrp) {
+        this.nrp = nrp;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public String getTanggalLahir() {
+        return tanggalLahir;
+    }
+
+    public void setTanggalLahir(String tanggalLahir) {
+        this.tanggalLahir = tanggalLahir;
+    }
+
+}
